7525ffedfc155b579caaf8ae6b28c56857fefacb
ducdec <nguyenvanduc011209@gmail.com>
2023-11-17 20:01:54 +0700

    Sua o day

--- src/pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h2>Home pages</h2>
-    </div>
-  );
-}
-
-export default Home;
--- src/pages/Upload/index.js
+++
@@ -1,9 +0,0 @@
-function Upload() {
-  return (
-    <div>
-      <h2>Upload pages</h2>
-    </div>
-  );
-}
-
-export default Upload;
